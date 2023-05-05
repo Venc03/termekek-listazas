a43b7c691a57c840c025d8e9967d262dfd874085
Venc03 <vegi.dani1@gmail.com>
2023-05-05 10:04:20 +0200

    major changes, accually dolgoztam oran

--- Termek.js
+++ Termek.js
@@ -1,5 +1,24 @@
 class Termek {
+    #adat;
+    #divElem;
+    #buttonElem;
     constructor(adat, szuloElem){
-        this.adat = adat;
+        this.#adat = adat;
+        szuloElem.append(
+            `<div><p>${this.#adat.nev}</p><p>${this.#adat.leiras}</p><p>${this.#adat.ar}</p><button>Kedvencekhez ad <br> +++</button></div>`
+        );
+        this.#divElem = szuloElem.children("div")
+        this.#buttonElem = this.szuloElem.children("button")
+    
+        this.#buttonElem.on("click", () => {
+            this.clickon()
+        });
+    }
+
+    clickon(){
+        const e = new Termek("elemclick", {detail:this.adat});
+        window.dispatchEvent(e);
     }
 }
+
+export default Termek;
--- Termekek.js
+++ Termekek.js
@@ -1,2 +1,20 @@
-import { LISTA } from "./adat";
+import { LISTA } from "./adat.js";
+import Termek from "./termek.js";
 
+
+class Termekek {
+    #kedvencek = [];
+
+    constructor() {
+        const SZULOELEM = $("article");
+
+        for (let i = 0; i < LISTA.length; i++){
+            const t1 = new Termek(LISTA[i], SZULOELEM)
+        }
+        $(window).on("click", (event) => {
+            this.#kedvencek.push(event.detail.adat);
+        });
+    }
+}
+
+export default Termekek;
